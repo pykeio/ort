9c77d4d020bfbcb7eaf41d0ac1afa115f3d1612c
Carson M <carson@pyke.io>
2023-01-17 11:42:07 -0600

    examples(gpt): refactor to use `tokenizers`

--- examples/gpt.rs
+++ examples/gpt.rs
@@ -2,12 +2,13 @@ use std::sync::Arc;
 
 use ndarray::{array, concatenate, s, Array1, Axis};
 use ort::{
-	download::language::{machine_comprehension::GPT2, MachineComprehension},
+	download::language::machine_comprehension::GPT2,
 	tensor::{DynOrtTensor, FromArray, InputTensor, OrtOwnedTensor},
-	Environment, ExecutionProvider, GraphOptimizationLevel, LoggingLevel, OrtResult, SessionBuilder
+	Environment, ExecutionProvider, GraphOptimizationLevel, OrtResult, SessionBuilder
 };
 use rand::Rng;
-use rust_tokenizers::{tokenizer::Gpt2Tokenizer, tokenizer::Tokenizer};
+use tokenizers::Tokenizer;
+
 const GEN_TOKENS: i32 = 45;
 const TOP_K: usize = 5;
 
@@ -17,26 +18,20 @@ fn main() -> OrtResult<()> {
 	let environment = Arc::new(
 		Environment::builder()
 			.with_name("GPT-2")
-			.with_log_level(LoggingLevel::Warning)
-			.with_execution_providers([ExecutionProvider::cuda_if_available()])
+			.with_execution_providers([ExecutionProvider::cuda()])
 			.build()?
 	);
 
 	let session = SessionBuilder::new(&environment)?
 		.with_optimization_level(GraphOptimizationLevel::Level1)?
 		.with_intra_threads(1)?
-		.with_model_downloaded(MachineComprehension::GPT2(GPT2::GPT2LmHead))?;
+		.with_model_downloaded(GPT2::GPT2LmHead)?;
 
-	let tokenizer = Gpt2Tokenizer::from_file("tests/data/gpt2/vocab.json", "tests/data/gpt2/merges.txt", false).unwrap();
+	let tokenizer = Tokenizer::from_file("tests/data/gpt2-tokenizer.json").unwrap();
 	let tokens = tokenizer
-		.encode(
-			"The corsac fox (Vulpes corsac), also known simply as a corsac, is a medium-sized fox found in",
-			None,
-			128,
-			&rust_tokenizers::tokenizer::TruncationStrategy::LongestFirst,
-			0
-		)
-		.token_ids;
+		.encode("The corsac fox (Vulpes corsac), also known simply as a corsac, is a medium-sized fox found in", false)
+		.unwrap();
+	let tokens = tokens.get_ids().iter().map(|i| *i as i64).collect::<Vec<_>>();
 
 	let tokens = &mut Array1::from_iter(tokens.iter().cloned());
 
@@ -59,7 +54,7 @@ fn main() -> OrtResult<()> {
 
 		let token = probabilities[rng.gen_range(0..=TOP_K)].0;
 		*tokens = concatenate![Axis(0), *tokens, array![token.try_into().unwrap()]];
-		let sentence = tokenizer.decode(&tokens.iter().copied().collect::<Vec<_>>(), true, true);
+		let sentence = tokenizer.decode(tokens.iter().map(|i| *i as u32).collect::<Vec<_>>(), true).unwrap();
 		println!("{}", sentence);
 	}
 

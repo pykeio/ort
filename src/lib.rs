2129797660581cacba8c767d7b936411bb1713f7
Carson M <carson@pyke.io>
2023-08-16 12:57:57 -0500

    fix: update version compatibility check for 1.15

--- src/lib.rs
+++ src/lib.rs
@@ -98,13 +98,13 @@ lazy_static! {
 			let version_string = get_version_string();
 			let version_string = CStr::from_ptr(version_string).to_string_lossy();
 			let lib_minor_version = version_string.split('.').nth(1).map(|x| x.parse::<u32>().unwrap_or(0)).unwrap_or(0);
-			match lib_minor_version.cmp(&14) {
+			match lib_minor_version.cmp(&15) {
 				std::cmp::Ordering::Less => panic!(
-					"ort 1.14 is not compatible with the ONNX Runtime binary found at `{}`; expected GetVersionString to return '1.14.x', but got '{version_string}'",
+					"ort 1.15 is not compatible with the ONNX Runtime binary found at `{}`; expected GetVersionString to return '1.15.x', but got '{version_string}'",
 					**G_ORT_DYLIB_PATH
 				),
 				std::cmp::Ordering::Greater => warn!(
-					"ort 1.14 may have compatibility issues with the ONNX Runtime binary found at `{}`; expected GetVersionString to return '1.14.x', but got '{version_string}'",
+					"ort 1.15 may have compatibility issues with the ONNX Runtime binary found at `{}`; expected GetVersionString to return '1.15.x', but got '{version_string}'",
 					**G_ORT_DYLIB_PATH
 				),
 				std::cmp::Ordering::Equal => {}

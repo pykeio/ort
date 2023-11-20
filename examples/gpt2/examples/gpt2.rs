3838303b2b8039ae519ad52354b201fb1b1d96bd
Carson M. <carson@pyke.io>
2023-11-19 23:41:00 -0600

    style(gpt2): convert doc comments to regular comments

--- examples/gpt2/examples/gpt2.rs
+++ examples/gpt2/examples/gpt2.rs
@@ -20,13 +20,13 @@ const TOP_K: usize = 5;
 /// The program initializes the model, tokenizes a prompt, and generates a sequence of tokens.
 /// It utilizes top-k sampling for diverse and contextually relevant text generation.
 fn main() -> ort::Result<()> {
-	/// Initialize tracing to receive debug messages from `ort`
+	// Initialize tracing to receive debug messages from `ort`
 	tracing_subscriber::fmt::init();
 
 	let mut stdout = io::stdout();
 	let mut rng = rand::thread_rng();
 
-	/// Create the ONNX Runtime environment and session for the GPT-2 model.
+	// Create the ONNX Runtime environment and session for the GPT-2 model.
 	let environment = Environment::builder()
 		.with_name("GPT-2")
 		.with_execution_providers([CUDAExecutionProvider::default().build()])
@@ -38,7 +38,7 @@ fn main() -> ort::Result<()> {
 		.with_intra_threads(1)?
 		.with_model_downloaded(GPT2::GPT2LmHead)?;
 
-	/// Load the tokenizer and encode the prompt into a sequence of tokens.
+	// Load the tokenizer and encode the prompt into a sequence of tokens.
 	let tokenizer = Tokenizer::from_file(Path::new(env!("CARGO_MANIFEST_DIR")).join("data").join("tokenizer.json")).unwrap();
 	let tokens = tokenizer.encode(PROMPT, false).unwrap();
 	let tokens = tokens.get_ids().iter().map(|i| *i as i64).collect::<Vec<_>>();
@@ -54,7 +54,7 @@ fn main() -> ort::Result<()> {
 		let generated_tokens: Tensor<f32> = outputs["output1"].extract_tensor()?;
 		let generated_tokens = generated_tokens.view();
 
-		/// Collect and sort logits 
+		// Collect and sort logits
 		let probabilities = &mut generated_tokens
 			.slice(s![0, 0, -1, ..])
 			.insert_axis(Axis(0))
@@ -65,7 +65,7 @@ fn main() -> ort::Result<()> {
 			.collect::<Vec<_>>();
 		probabilities.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap_or(std::cmp::Ordering::Less));
 
-		/// Sample using top-k sampling
+		// Sample using top-k sampling
 		let token = probabilities[rng.gen_range(0..=TOP_K)].0;
 		tokens = concatenate![Axis(0), tokens, array![token.try_into().unwrap()]];
 

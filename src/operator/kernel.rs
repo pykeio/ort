1d8b815f4fa8f08b3fb268b2efc59fc16b5773aa
Carson M. <carson@pyke.io>
2024-05-06 14:09:18 -0500

    feat: implement more kernel attribute types
    still need constant inputs and functions for getting I/O lengths & names from indexes

--- src/operator/kernel.rs
+++ src/operator/kernel.rs
@@ -1,9 +1,9 @@
 use std::{
-	ffi::CString,
+	ffi::{c_char, CString},
 	ptr::{self, NonNull}
 };
 
-use crate::{error::status_to_result, ortsys, value::ValueRefMut, Error, Result, Value, ValueRef};
+use crate::{error::status_to_result, ortsys, value::ValueRefMut, Allocator, DowncastableTarget, DynValue, Error, Result, Value, ValueRef};
 
 pub trait Kernel {
 	fn compute(&mut self, ctx: &KernelContext) -> crate::Result<()>;
@@ -25,19 +25,19 @@ impl KernelAttributes {
 	}
 
 	#[allow(private_bounds)]
-	pub fn get<T: GetKernelAttribute>(&self, name: impl AsRef<str>) -> Option<T> {
+	pub fn get<'s, T: GetKernelAttribute<'s>>(&'s self, name: impl AsRef<str>) -> Option<T> {
 		let name = CString::new(name.as_ref()).ok()?;
 		T::get_from(self.0.as_ptr(), name.as_ptr())
 	}
 }
 
-pub trait GetKernelAttribute {
+pub trait GetKernelAttribute<'s> {
 	fn get_from(info: *mut ort_sys::OrtKernelInfo, name: *const ort_sys::c_char) -> Option<Self>
 	where
 		Self: Sized;
 }
 
-impl GetKernelAttribute for f32 {
+impl<'s> GetKernelAttribute<'s> for f32 {
 	fn get_from(info: *mut ort_sys::OrtKernelInfo, name: *const ort_sys::c_char) -> Option<Self>
 	where
 		Self: Sized
@@ -45,6 +45,73 @@ impl GetKernelAttribute for f32 {
 		let mut value = Self::default();
 		status_to_result(ortsys![unsafe KernelInfoGetAttribute_float(info, name, &mut value)]).ok()?;
 		Some(value)
+	}
+}
+
+impl<'s> GetKernelAttribute<'s> for i64 {
+	fn get_from(info: *mut ort_sys::OrtKernelInfo, name: *const ort_sys::c_char) -> Option<Self>
+	where
+		Self: Sized
+	{
+		let mut value = Self::default();
+		status_to_result(ortsys![unsafe KernelInfoGetAttribute_int64(info, name, &mut value)]).ok()?;
+		Some(value)
+	}
+}
+
+impl<'s> GetKernelAttribute<'s> for String {
+	fn get_from(info: *mut ort_sys::OrtKernelInfo, name: *const ort_sys::c_char) -> Option<Self>
+	where
+		Self: Sized
+	{
+		let mut size = ort_sys::size_t::default();
+		status_to_result(ortsys![unsafe KernelInfoGetAttribute_string(info, name, ptr::null_mut(), &mut size)]).ok()?;
+		let mut out = vec![0u8; size];
+		status_to_result(ortsys![unsafe KernelInfoGetAttribute_string(info, name, out.as_mut_ptr().cast::<c_char>(), &mut size)]).ok()?;
+		String::from_utf8(out).ok()
+	}
+}
+
+impl<'s> GetKernelAttribute<'s> for Vec<f32> {
+	fn get_from(info: *mut ort_sys::OrtKernelInfo, name: *const ort_sys::c_char) -> Option<Self>
+	where
+		Self: Sized
+	{
+		let mut size = ort_sys::size_t::default();
+		status_to_result(ortsys![unsafe KernelInfoGetAttributeArray_float(info, name, ptr::null_mut(), &mut size)]).ok()?;
+		let mut out = vec![0f32; size];
+		status_to_result(ortsys![unsafe KernelInfoGetAttributeArray_float(info, name, out.as_mut_ptr(), &mut size)]).ok()?;
+		Some(out)
+	}
+}
+
+impl<'s> GetKernelAttribute<'s> for Vec<i64> {
+	fn get_from(info: *mut ort_sys::OrtKernelInfo, name: *const ort_sys::c_char) -> Option<Self>
+	where
+		Self: Sized
+	{
+		let mut size = ort_sys::size_t::default();
+		status_to_result(ortsys![unsafe KernelInfoGetAttributeArray_int64(info, name, ptr::null_mut(), &mut size)]).ok()?;
+		let mut out = vec![0i64; size];
+		status_to_result(ortsys![unsafe KernelInfoGetAttributeArray_int64(info, name, out.as_mut_ptr(), &mut size)]).ok()?;
+		Some(out)
+	}
+}
+
+impl<'s, T: DowncastableTarget> GetKernelAttribute<'s> for ValueRef<'s, T> {
+	fn get_from(info: *mut ort_sys::OrtKernelInfo, name: *const ort_sys::c_char) -> Option<Self>
+	where
+		Self: Sized
+	{
+		// TODO: This should probably be customizable - docs say the allocator is required for "internal tensor state", but it's
+		// not clear if this also includes tensor data (and thus it should instead be allocated on an appropriate device).
+		let allocator = Allocator::default();
+
+		let mut value_ptr: *mut ort_sys::OrtValue = ptr::null_mut();
+		status_to_result(ortsys![unsafe KernelInfoGetAttribute_tensor(info, name, allocator.ptr.as_ptr(), &mut value_ptr)]).ok()?;
+		unsafe { ValueRef::new(DynValue::from_ptr(NonNull::new(value_ptr)?, None)) }
+			.downcast()
+			.ok()
 	}
 }
 

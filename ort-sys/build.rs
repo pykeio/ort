60f6ecae9064a75a474678998af42ea87067b1b1
Carson M. <carson@pyke.io>
2024-09-05 00:17:56 -0500

    chore: update to ONNX Runtime v1.19.2

--- ort-sys/build.rs
+++ ort-sys/build.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 #[allow(unused)]
-const ONNXRUNTIME_VERSION: &str = "1.19.0";
+const ONNXRUNTIME_VERSION: &str = "1.19.2";
 
 const ORT_ENV_SYSTEM_LIB_LOCATION: &str = "ORT_LIB_LOCATION";
 const ORT_ENV_SYSTEM_LIB_PROFILE: &str = "ORT_LIB_PROFILE";

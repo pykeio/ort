3133a9322b5bd9364b54f99ff8ee3ca80b4e1254
Carson M. <carson@pyke.io>
2025-02-22 21:20:08 -0600

    refactor: use rocm v2 registration

--- src/execution_providers/rocm.rs
+++ src/execution_providers/rocm.rs
@@ -1,118 +1,99 @@
-use alloc::format;
+use alloc::{format, string::ToString};
 use core::ffi::c_void;
 
+use super::{ArbitrarilyConfigurableExecutionProvider, ExecutionProviderOptions};
 use crate::{
 	error::{Error, Result},
 	execution_providers::{ArenaExtendStrategy, ExecutionProvider, ExecutionProviderDispatch},
 	session::builder::SessionBuilder
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone)]
 pub struct ROCmExecutionProvider {
-	device_id: i32,
-	miopen_conv_exhaustive_search: bool,
-	gpu_mem_limit: usize,
-	arena_extend_strategy: ArenaExtendStrategy,
-	do_copy_in_default_stream: bool,
-	user_compute_stream: Option<*mut c_void>,
-	default_memory_arena_cfg: Option<*mut ort_sys::OrtArenaCfg>,
-	enable_hip_graph: bool,
-	tunable_op_enable: bool,
-	tunable_op_tuning_enable: bool,
-	tunable_op_max_tuning_duration_ms: i32
-}
-
-unsafe impl Send for ROCmExecutionProvider {}
-unsafe impl Sync for ROCmExecutionProvider {}
-
-impl Default for ROCmExecutionProvider {
-	fn default() -> Self {
-		Self {
-			device_id: 0,
-			miopen_conv_exhaustive_search: false,
-			gpu_mem_limit: usize::MAX,
-			arena_extend_strategy: ArenaExtendStrategy::NextPowerOfTwo,
-			do_copy_in_default_stream: true,
-			user_compute_stream: None,
-			default_memory_arena_cfg: None,
-			enable_hip_graph: false,
-			tunable_op_enable: false,
-			tunable_op_tuning_enable: false,
-			tunable_op_max_tuning_duration_ms: 0
-		}
-	}
+	options: ExecutionProviderOptions
 }
 
 impl ROCmExecutionProvider {
 	#[must_use]
 	pub fn with_device_id(mut self, device_id: i32) -> Self {
-		self.device_id = device_id;
+		self.options.set("device_id", device_id.to_string());
 		self
 	}
 
 	#[must_use]
 	pub fn with_exhaustive_conv_search(mut self, enable: bool) -> Self {
-		self.miopen_conv_exhaustive_search = enable;
+		self.options.set("miopen_conv_exhaustive_search", if enable { "1" } else { "0" });
+		self
+	}
+
+	#[must_use]
+	pub fn with_conv_use_max_workspace(mut self, enable: bool) -> Self {
+		self.options.set("miopen_conv_use_max_workspace", if enable { "1" } else { "0" });
 		self
 	}
 
 	#[must_use]
 	pub fn with_mem_limit(mut self, limit: usize) -> Self {
-		self.gpu_mem_limit = limit;
+		self.options.set("gpu_mem_limit", limit.to_string());
 		self
 	}
 
 	#[must_use]
 	pub fn with_arena_extend_strategy(mut self, strategy: ArenaExtendStrategy) -> Self {
-		self.arena_extend_strategy = strategy;
+		self.options.set("arena_extend_strategy", match strategy {
+			ArenaExtendStrategy::NextPowerOfTwo => "kNextPowerOfTwo",
+			ArenaExtendStrategy::SameAsRequested => "kSameAsRequested"
+		});
 		self
 	}
 
 	#[must_use]
 	pub fn with_copy_in_default_stream(mut self, enable: bool) -> Self {
-		self.do_copy_in_default_stream = enable;
+		self.options.set("do_copy_in_default_stream", if enable { "1" } else { "0" });
 		self
 	}
 
 	#[must_use]
 	pub fn with_compute_stream(mut self, ptr: *mut c_void) -> Self {
-		self.user_compute_stream = Some(ptr);
-		self
-	}
-
-	#[must_use]
-	pub fn with_default_memory_arena_cfg(mut self, cfg: *mut ort_sys::OrtArenaCfg) -> Self {
-		self.default_memory_arena_cfg = Some(cfg);
+		self.options.set("has_user_compute_stream", "1");
+		self.options.set("user_compute_stream", (ptr as usize).to_string());
 		self
 	}
 
 	#[must_use]
 	pub fn with_hip_graph(mut self, enable: bool) -> Self {
-		self.enable_hip_graph = enable;
+		self.options.set("enable_hip_graph", if enable { "1" } else { "0" });
 		self
 	}
 
 	#[must_use]
 	pub fn with_tunable_op(mut self, enable: bool) -> Self {
-		self.tunable_op_enable = enable;
+		self.options.set("tunable_op_enable", if enable { "1" } else { "0" });
 		self
 	}
 
 	#[must_use]
 	pub fn with_tuning(mut self, enable: bool) -> Self {
-		self.tunable_op_tuning_enable = enable;
+		self.options.set("tunable_op_tuning_enable", if enable { "1" } else { "0" });
 		self
 	}
 
 	#[must_use]
 	pub fn with_max_tuning_duration(mut self, ms: i32) -> Self {
-		self.tunable_op_max_tuning_duration_ms = ms;
+		self.options.set("tunable_op_max_tuning_duration_ms", ms.to_string());
 		self
 	}
 
 	#[must_use]
 	pub fn build(self) -> ExecutionProviderDispatch {
 		self.into()
+	}
+}
+
+impl ArbitrarilyConfigurableExecutionProvider for ROCmExecutionProvider {
+	fn with_arbitrary_config(mut self, key: impl ToString, value: impl ToString) -> Self {
+		self.options.set(key.to_string(), value.to_string());
+		self
 	}
 }
 
@@ -135,29 +116,50 @@ impl ExecutionProvider for ROCmExecutionProvider {
 	fn register(&self, session_builder: &mut SessionBuilder) -> Result<()> {
 		#[cfg(any(feature = "load-dynamic", feature = "rocm"))]
 		{
-			use core::ptr;
-
 			use crate::AsPointer;
 
-			let rocm_options = ort_sys::OrtROCMProviderOptions {
-				device_id: self.device_id,
-				miopen_conv_exhaustive_search: self.miopen_conv_exhaustive_search.into(),
-				gpu_mem_limit: self.gpu_mem_limit,
-				arena_extend_strategy: match self.arena_extend_strategy {
-					ArenaExtendStrategy::NextPowerOfTwo => 0,
-					ArenaExtendStrategy::SameAsRequested => 1
-				},
-				do_copy_in_default_stream: self.do_copy_in_default_stream.into(),
-				has_user_compute_stream: self.user_compute_stream.is_some().into(),
-				user_compute_stream: self.user_compute_stream.unwrap_or_else(ptr::null_mut),
-				default_memory_arena_cfg: self.default_memory_arena_cfg.unwrap_or_else(ptr::null_mut),
-				enable_hip_graph: self.enable_hip_graph.into(),
-				tunable_op_enable: self.tunable_op_enable.into(),
-				tunable_op_tuning_enable: self.tunable_op_tuning_enable.into(),
-				tunable_op_max_tuning_duration_ms: self.tunable_op_max_tuning_duration_ms
-			};
-			crate::ortsys![unsafe SessionOptionsAppendExecutionProvider_ROCM(session_builder.ptr_mut(), ptr::addr_of!(rocm_options))?];
-			return Ok(());
+			let mut rocm_options: *mut ort_sys::OrtROCMProviderOptions = core::ptr::null_mut();
+			crate::ortsys![unsafe CreateROCMProviderOptions(&mut rocm_options)?];
+			let ffi_options = self.options.to_ffi();
+
+			let res = crate::ortsys![unsafe UpdateROCMProviderOptions(
+				rocm_options,
+				ffi_options.key_ptrs(),
+				ffi_options.value_ptrs(),
+				ffi_options.len()
+			)];
+			if let Err(e) = unsafe { crate::error::status_to_result(res) } {
+				crate::ortsys![unsafe ReleaseROCMProviderOptions(rocm_options)];
+				return Err(e);
+			}
+
+			let status = crate::ortsys![unsafe SessionOptionsAppendExecutionProvider_ROCM(session_builder.ptr_mut(), rocm_options)];
+			crate::ortsys![unsafe ReleaseROCMProviderOptions(rocm_options)];
+			return unsafe { crate::error::status_to_result(status) };
+
+			// use core::ptr;
+
+			// use crate::AsPointer;
+
+			// let rocm_options = ort_sys::OrtROCMProviderOptions {
+			// 	device_id: self.device_id,
+			// 	miopen_conv_exhaustive_search: self.miopen_conv_exhaustive_search.into(),
+			// 	gpu_mem_limit: self.gpu_mem_limit,
+			// 	arena_extend_strategy: match self.arena_extend_strategy {
+			// 		ArenaExtendStrategy::NextPowerOfTwo => 0,
+			// 		ArenaExtendStrategy::SameAsRequested => 1
+			// 	},
+			// 	do_copy_in_default_stream: self.do_copy_in_default_stream.into(),
+			// 	has_user_compute_stream: self.user_compute_stream.is_some().into(),
+			// 	user_compute_stream: self.user_compute_stream.unwrap_or_else(ptr::null_mut),
+			// 	default_memory_arena_cfg: self.default_memory_arena_cfg.unwrap_or_else(ptr::null_mut),
+			// 	enable_hip_graph: self.enable_hip_graph.into(),
+			// 	tunable_op_enable: self.tunable_op_enable.into(),
+			// 	tunable_op_tuning_enable: self.tunable_op_tuning_enable.into(),
+			// 	tunable_op_max_tuning_duration_ms: self.tunable_op_max_tuning_duration_ms
+			// };
+			// crate::ortsys![unsafe SessionOptionsAppendExecutionProvider_ROCM(session_builder.ptr_mut(),
+			// ptr::addr_of!(rocm_options))?]; return Ok(());
 		}
 
 		Err(Error::new(format!("`{}` was not registered because its corresponding Cargo feature is not enabled.", self.as_str())))

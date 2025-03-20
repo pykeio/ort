d56bf4a0b66e55727959657d65aabf2d388f1ac2
Carson M. <carson@pyke.io>
2025-03-19 23:32:36 -0500

    variadic op test

--- src/operator/tests.rs
+++ src/operator/tests.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::{
 	Result,
 	operator::{
@@ -191,6 +193,123 @@ fn test_op_attrs() -> crate::Result<()> {
 
 	let values = session.run(crate::inputs!["input_0" => &value1])?;
 	assert_eq!(values[0].try_extract_raw_tensor::<f32>()?.1, [6.0, 12.0, 18.0, 24.0, 30.0]);
+
+	Ok(())
+}
+
+struct CopyTensorArrayAllVariadic;
+
+impl Operator for CopyTensorArrayAllVariadic {
+	fn name(&self) -> &str {
+		"CopyTensorArrayAllVariadic"
+	}
+
+	fn inputs(&self) -> Vec<OperatorInput> {
+		vec![OperatorInput::variadic(1).homogenous(TensorElementType::Float32)]
+	}
+
+	fn outputs(&self) -> Vec<OperatorOutput> {
+		vec![OperatorOutput::variadic(1).homogenous(TensorElementType::Float32)]
+	}
+
+	fn infer_shape(&self, ctx: &mut super::ShapeInferenceContext) -> crate::Result<()> {
+		for (i, input) in ctx.inputs().into_iter().enumerate() {
+			ctx.set_output(i, &input)?;
+		}
+
+		Ok(())
+	}
+
+	fn create_kernel(&self, _: &KernelAttributes) -> crate::Result<Box<dyn Kernel>> {
+		Ok(Box::new(|ctx: &KernelContext| copy_variadic(0, ctx)))
+	}
+}
+
+fn copy_variadic(start: usize, ctx: &KernelContext) -> crate::Result<()> {
+	for i in start..ctx.num_inputs()? {
+		let input = ctx.input(i)?.ok_or_else(|| crate::Error::new("missing input"))?;
+		let mut output = ctx
+			.output(i, input.shape().clone())?
+			.ok_or_else(|| crate::Error::new(format!("failed to allocate output {i}")))?;
+
+		output
+			.try_extract_raw_tensor_mut::<f32>()?
+			.1
+			.copy_from_slice(input.try_extract_raw_tensor()?.1);
+	}
+	Ok(())
+}
+
+struct CopyTensorArrayCombined;
+
+impl Operator for CopyTensorArrayCombined {
+	fn name(&self) -> &str {
+		"CopyTensorArrayCombined"
+	}
+
+	fn inputs(&self) -> Vec<OperatorInput> {
+		vec![OperatorInput::optional(TensorElementType::Float32), OperatorInput::variadic(0).homogenous(TensorElementType::Float32)]
+	}
+
+	fn outputs(&self) -> Vec<OperatorOutput> {
+		vec![OperatorOutput::optional(TensorElementType::Float32), OperatorOutput::variadic(0).homogenous(TensorElementType::Float32)]
+	}
+
+	fn infer_shape(&self, ctx: &mut super::ShapeInferenceContext) -> crate::Result<()> {
+		for (i, input) in ctx.inputs().into_iter().enumerate() {
+			ctx.set_output(i, &input)?;
+		}
+
+		Ok(())
+	}
+
+	fn create_kernel(&self, _: &KernelAttributes) -> crate::Result<Box<dyn Kernel>> {
+		Ok(Box::new(|ctx: &KernelContext| {
+			if let Ok(Some(input)) = ctx.input(0) {
+				let mut output = ctx
+					.output(0, input.shape().clone())?
+					.ok_or_else(|| crate::Error::new("failed to allocate output 0"))?;
+
+				output
+					.try_extract_raw_tensor_mut::<f32>()?
+					.1
+					.copy_from_slice(input.try_extract_raw_tensor()?.1);
+			}
+			copy_variadic(1, ctx)
+		}))
+	}
+}
+
+#[test]
+fn test_variadic_io() -> crate::Result<()> {
+	let ops = Arc::new(
+		OperatorDomain::new("test.customop")?
+			.add(CopyTensorArrayAllVariadic)?
+			.add(CopyTensorArrayCombined)?
+	);
+
+	let model = std::fs::read("tests/data/copy_2_inputs_2_outputs.onnx").expect("");
+	let mut session = Session::builder()?.with_operators(Arc::clone(&ops))?.commit_from_memory(&model)?;
+
+	let input0 = Tensor::from_array(([15], vec![1.1_f32, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0, 11.1, 12.2, 13.3, 14.4, 15.5]))?;
+	let input1 = Tensor::from_array(([15], vec![15.5_f32, 14.4, 13.3, 12.2, 11.1, 10.0, 9.9, 8.8, 7.7, 6.6, 5.5, 4.4, 3.3, 2.2, 1.1]))?;
+
+	let values = session.run(crate::inputs!["input_0" => &input0, "input_1" => &input1])?;
+	assert_eq!(values[0].try_extract_raw_tensor::<f32>()?.1, input0.extract_raw_tensor().1);
+	assert_eq!(values[1].try_extract_raw_tensor::<f32>()?.1, input1.extract_raw_tensor().1);
+
+	let model = std::fs::read("tests/data/copy_3_inputs_3_outputs.onnx").expect("");
+	let mut session = Session::builder()?.with_operators(Arc::clone(&ops))?.commit_from_memory(&model)?;
+
+	let input2 = Tensor::from_array(([15], vec![6.6_f32, 7.7, 8.8, 9.9, 10.0, 1.1, 2.2, 3.3, 4.4, 5.5, 11.1, 12.2, 13.3, 14.4, 15.5]))?;
+	let values = session.run(crate::inputs![
+		"input_0" => &input0,
+		"input_1" => &input1,
+		"input_2" => &input2
+	])?;
+	assert_eq!(values[0].try_extract_raw_tensor::<f32>()?.1, input0.extract_raw_tensor().1);
+	assert_eq!(values[1].try_extract_raw_tensor::<f32>()?.1, input1.extract_raw_tensor().1);
+	assert_eq!(values[2].try_extract_raw_tensor::<f32>()?.1, input2.extract_raw_tensor().1);
 
 	Ok(())
 }

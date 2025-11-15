64f76d2b79eb01cd718ca636aa2d4bfb6d80a72f
Carson M. <carson@pyke.io>
2025-11-14 22:25:30 -0600

    fix(sys): some warnings when building with `--no-default-features`

--- ort-sys/build/dynamic_link.rs
+++ ort-sys/build/dynamic_link.rs
@@ -1,6 +1,7 @@
-use std::{fs, path::Path};
+#[cfg(feature = "copy-dylibs")]
+use std::path::Path;
 
-use crate::{log, vars};
+use crate::vars;
 
 pub fn prefer_dynamic_linking() -> bool {
 	match vars::get(vars::PREFER_DYNAMIC_LINK) {
@@ -9,7 +10,10 @@ pub fn prefer_dynamic_linking() -> bool {
 	}
 }
 
+#[cfg(feature = "copy-dylibs")]
 pub fn copy_dylibs(lib_dir: &Path, out_dir: &Path) {
+	use std::fs;
+
 	// get the target directory - we need to place the dlls next to the executable so they can be properly loaded by windows
 	let out_dir = out_dir.ancestors().nth(3).unwrap();
 	for out_dir in [out_dir.to_path_buf(), out_dir.join("examples"), out_dir.join("deps")] {
@@ -18,7 +22,7 @@ pub fn copy_dylibs(lib_dir: &Path, out_dir: &Path) {
 		#[cfg(not(windows))]
 		let copy_fallback = false;
 
-		let lib_files = std::fs::read_dir(lib_dir).unwrap_or_else(|_| panic!("Failed to read contents of `{}` (does it exist?)", lib_dir.display()));
+		let lib_files = fs::read_dir(lib_dir).unwrap_or_else(|_| panic!("Failed to read contents of `{}` (does it exist?)", lib_dir.display()));
 		for lib_file in lib_files.filter(|e| {
 			e.as_ref().ok().is_some_and(|e| {
 				e.file_type().is_ok_and(|e| !e.is_dir()) && [".dll", ".so", ".dylib"].into_iter().any(|v| e.path().to_string_lossy().contains(v))
@@ -47,7 +51,7 @@ pub fn copy_dylibs(lib_dir: &Path, out_dir: &Path) {
 
 		// If we had to fallback to copying files on Windows, break early to avoid copying to 3 different directories
 		if copy_fallback {
-			log::warning!(
+			crate::log::warning!(
 				"had to copy dylibs because Windows Developer Mode is not enabled, or the cache dir is on a different drive. examples & tests will not be able to access the dylibs"
 			);
 			break;

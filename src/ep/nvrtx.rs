8c13549862bb5b544cceff61965146d4fdfb581d
Carson M. <carson@pyke.io>
2025-11-30 13:11:30 -0600

    feat: add missing options for NVRTX EP

--- src/ep/nvrtx.rs
+++ src/ep/nvrtx.rs
@@ -11,14 +11,68 @@ pub struct NVRTX {
 super::impl_ep!(arbitrary; NVRTX);
 
 impl NVRTX {
+	#[must_use]
 	pub fn with_device_id(mut self, device_id: u32) -> Self {
-		self.options.set("ep.nvtensorrtrtxexecutionprovider.device_id", device_id.to_string());
+		self.options.set("device_id", device_id.to_string());
 		self
 	}
 
+	/// Use a custom CUDA device stream rather than the default one.
+	///
+	/// # Safety
+	/// The provided `stream` must outlive the environment/session configured to use this execution provider.
+	#[must_use]
+	pub unsafe fn with_compute_stream(mut self, stream: *mut ()) -> Self {
+		self.options.set("has_user_compute_stream", "1");
+		self.options.set("user_compute_stream", (stream as usize).to_string());
+		self
+	}
+
+	#[must_use]
 	pub fn with_cuda_graph(mut self, enable: bool) -> Self {
-		self.options
-			.set("ep.nvtensorrtrtxexecutionprovider.nv_cuda_graph_enable", if enable { "1" } else { "0" });
+		self.options.set("enable_cuda_graph", if enable { "1" } else { "0" });
+		self
+	}
+
+	#[must_use]
+	pub fn with_max_workspace(mut self, limit: usize) -> Self {
+		self.options.set("nv_max_workspace_size", limit.to_string());
+		self
+	}
+
+	#[must_use]
+	pub fn with_max_shared_mem(mut self, limit: usize) -> Self {
+		self.options.set("nv_max_shared_mem_size", limit.to_string());
+		self
+	}
+
+	#[must_use]
+	pub fn with_profile_min_shapes(mut self, shapes: impl ToString) -> Self {
+		self.options.set("nv_profile_min_shapes", shapes.to_string());
+		self
+	}
+
+	#[must_use]
+	pub fn with_profile_max_shapes(mut self, shapes: impl ToString) -> Self {
+		self.options.set("nv_profile_max_shapes", shapes.to_string());
+		self
+	}
+
+	#[must_use]
+	pub fn with_profile_opt_shapes(mut self, shapes: impl ToString) -> Self {
+		self.options.set("nv_profile_opt_shapes", shapes.to_string());
+		self
+	}
+
+	#[must_use]
+	pub fn with_multi_profile(mut self, enable: bool) -> Self {
+		self.options.set("nv_multi_profile_enable", if enable { "1" } else { "0" });
+		self
+	}
+
+	#[must_use]
+	pub fn with_runtime_cache_path(mut self, path: impl ToString) -> Self {
+		self.options.set("nv_runtime_cache_path", path.to_string());
 		self
 	}
 }

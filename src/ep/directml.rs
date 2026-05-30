726dc7c47d8e0b2548885f796062b0b6d32d892e
Carson M. <carson@pyke.io>
2026-05-30 03:08:23 -0500

    feat: expose DirectML API

--- src/ep/directml.rs
+++ src/ep/directml.rs
@@ -1,18 +1,17 @@
-use core::ptr;
+use core::{
+	ops::Deref,
+	ptr::{self, NonNull}
+};
 
 use super::{ExecutionProvider, RegisterError};
-use crate::{error::Result, ortsys, session::builder::SessionBuilder, util::OnceLock};
-
-#[allow(unused)]
-fn get_dml_api() -> Result<&'static ort_sys::OrtDmlApi> {
-	static DML_API: OnceLock<ort_sys::OrtDmlApi> = OnceLock::new();
-	DML_API.get_or_try_init(|| {
-		let mut ptr: *const ort_sys::c_void = ptr::null();
-		ortsys![unsafe GetExecutionProviderApi(c"DML".as_ptr(), 0, &mut ptr)?];
-		assert!(!ptr.is_null());
-		Ok(unsafe { (*ptr.cast::<ort_sys::OrtDmlApi>()).clone() })
-	})
-}
+use crate::{
+	AsPointer,
+	error::{Error, Result},
+	memory::Allocator,
+	ortsys,
+	session::builder::SessionBuilder,
+	util::OnceLock
+};
 
 #[derive(Default, Debug, Copy, Clone, Hash, PartialEq, Eq)]
 pub enum PerformancePreference {
@@ -102,9 +101,7 @@ impl ExecutionProvider for DirectML {
 	fn register(&self, session_builder: &mut SessionBuilder) -> Result<(), RegisterError> {
 		#[cfg(any(feature = "load-dynamic", feature = "directml"))]
 		{
-			use crate::{AsPointer, Error};
-
-			let api = get_dml_api()?;
+			let api = api().ok_or_else(|| Error::new("DirectML is not supported in this build of ONNX Runtime"))?;
 			if let Some(device_id) = self.device_id {
 				unsafe { Error::result_from_status((api.SessionOptionsAppendExecutionProvider_DML)(session_builder.ptr_mut(), device_id as _)) }?;
 			} else {
@@ -127,5 +124,69 @@ impl ExecutionProvider for DirectML {
 		}
 
 		Err(RegisterError::MissingFeature)
+	}
+}
+
+/// Returns the [`OrtDmlApi`](ort_sys::OrtDmlApi) if this build of ONNX Runtime supports DirectML.
+pub fn api() -> Option<&'static ort_sys::OrtDmlApi> {
+	static DML_API: OnceLock<&'static ort_sys::OrtDmlApi> = OnceLock::new();
+	DML_API
+		.get_or_try_init(|| {
+			let mut ptr: *const ort_sys::c_void = ptr::null();
+			ortsys![unsafe GetExecutionProviderApi(c"DML".as_ptr(), 0, &mut ptr)?; nonNull(ptr)];
+			let ptr: NonNull<*const ort_sys::c_void> = ptr; // weird type inference stuff
+			Ok(unsafe { ptr.cast::<ort_sys::OrtDmlApi>().as_ref() })
+		})
+		.ok()
+		.copied()
+}
+
+/// Creates a DirectML resource from a Direct3D resource.
+///
+/// # Safety
+/// `resource` must be a valid, non-null `ID3D12Resource`.
+pub unsafe fn resource_from_d3d(resource: *mut ()) -> Result<DMLResource> {
+	let mut ptr: *mut ort_sys::c_void = ptr::null_mut();
+	let dml_api = api().ok_or_else(|| Error::new("DirectML is not supported in this build of ONNX Runtime"))?;
+	unsafe { Error::result_from_status((dml_api.CreateGPUAllocationFromD3DResource)(resource.cast(), &mut ptr)) }?;
+	Ok(DMLResource(ptr))
+}
+
+#[repr(transparent)]
+#[derive(Debug)]
+pub struct DMLResource(*mut ort_sys::c_void);
+
+impl DMLResource {
+	pub fn d3d_resource(&self, allocator: &Allocator) -> Result<*mut ()> {
+		let mut ptr: *mut ort_sys::c_void = ptr::null_mut();
+		let dml_api = api().ok_or_else(|| Error::new("DirectML is not supported in this build of ONNX Runtime"))?;
+		unsafe { Error::result_from_status((dml_api.GetD3D12ResourceFromAllocation)(allocator.ptr().cast_mut(), self.0.cast(), &mut ptr)) }?;
+		Ok(ptr.cast())
+	}
+}
+
+impl Deref for DMLResource {
+	type Target = *mut ort_sys::c_void;
+
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
+}
+
+impl AsPointer for DMLResource {
+	type Sys = ();
+
+	fn ptr(&self) -> *const Self::Sys {
+		self.0.cast()
+	}
+}
+
+impl Drop for DMLResource {
+	fn drop(&mut self) {
+		let Some(dml_api) = api() else {
+			return;
+		};
+
+		let _ = unsafe { Error::result_from_status((dml_api.FreeGPUAllocation)(self.0)) };
 	}
 }
